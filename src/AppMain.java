b8cfcf916c2370ff51391efcac2dc271c8148fd7
arpitparekh <arpitparekh9@gmail.com>
2024-07-11 17:32:29 +0530

    first commit

---
+++ src/AppMain.java
@@ -0,0 +1,32 @@
+public class AppMain {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Wolrd!");
+
+        // variables
+        /*
+         * multi line comment
+         * 
+         */
+
+        int rollNumber = 12;
+        int age = 13;
+
+        // datatype variable_name = value
+
+        System.out.println(rollNumber + age);
+
+        // Camel Case   // AppMain (pascal case) // userName  // java
+
+        int a = 23;  // initilization and declaration in one statement
+
+        int b;  // declaration
+        b = 45; // initialization
+
+        int a1=12, b1=13, c=14, d1=15, f1=56;
+        System.out.println(a1);
+
+        
+
+    }
+}
---
+++ src/MyDatatypes.java
@@ -0,0 +1,45 @@
+public class MyDatatypes {
+    
+    public static void main(String[] args) {
+        
+        //  datatypes
+
+        // 8 primitive datatypes
+        
+        // Number datatype
+
+        byte b = 45; // 1 byte  // 1 bytes = 16 bit // 101010101010
+        System.out.println(b);
+        short s = 1; // 2 bytes  // 32 bit
+        System.out.println(s);
+
+        int i = 9888885; // 4 byte  // 64 bit  // 10101010101010101010101010110
+        System.out.println(i);
+
+        long l = 343434343434343434L; // long
+        System.out.println(l);
+
+        float f = 34.34f; // 4 byte
+        System.out.println(f);
+        
+        double d = 343434.34343434; // 8 byte
+        System.out.println(d);
+
+        char ch = '3';
+        System.out.println(ch);
+
+        boolean isLogin = false;
+        System.out.println(isLogin);
+
+        // stack  
+
+        // array
+
+        // object datatype
+        // String
+        // ArrayList 
+        // collection framework
+
+    }
+
+}
