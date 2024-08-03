ec7bd2f6f8d88860483be29c700ae6a40eef2605
arpitparekh <arpitparekh9@gmail.com>
2024-08-03 16:22:38 +0530

    static

--- src/oopc/MyKeywords.java
+++ src/oopc/MyKeywords.java
@@ -9,23 +9,64 @@ public class MyKeywords {
         // static
         // final
 
-        High h = new High();
-        High h = new High();
-        High h = new High();
+        High h = new High(177);
+        High h1 = new High(77);
+        High h2 = new High(67);
 
+        System.out.println(High.b);
+        System.out.println(h.a);
 
+        System.out.println();
 
+        System.out.println(High.b);
+        System.out.println(h1.a);
+
+        System.out.println();
+
+        System.out.println(High.b);
+        System.out.println(h2.a);
+
+        High.b = 45;
+
+        System.out.println(High.b);
+        System.out.println(h.a);
+
+        System.out.println();
+
+        System.out.println(High.b);
+        System.out.println(h1.a);
+
+        System.out.println();
+
+        System.out.println(High.b);
+        System.out.println(h2.a);
+
+        h1.display();
+
+        High.fun();
 
     }
 }
 
 class High {
 
+    static {                                      // singleton
+        System.out.println("Static block");
+    }
+
     static int b  = 12;
     int a = 12;
 
-    High(a) {
+    High(int a) {
         this.a = a;
+    }
+
+    void display() {
+        b = 23;
+    }
+
+    static void fun() {
+        System.out.println("Fun method");
     }
 
 }
